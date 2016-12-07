9f351fee74214b182c7e60278edde89caf7c9b99
Jack <Jack_Swenson@Baylor.edu>
2016-12-07 16:48:29 -0600

    broken

--- lib/api-gateway.service.ts
+++ lib/api-gateway.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-//import { BlogPost } from '../blog/blog-post';
+import { HttpClient } from "./http-client";
 import { AwsService } from './aws.service';
 import { ApiClientService } from './api-client.service';
 import { Regex } from './regex.service';
 
 @Injectable()
-export class ApiGatewayService {
+export class ApiGatewayService implements HttpClient {
 
   client = "";
   authenticating;
@@ -55,16 +55,21 @@ export class ApiGatewayService {
     return method.substr(method.length - 6) === 'Delete';
   }
 
-  post(endpoint) {
-
-  }
-
-  get(endpoint) {
-
-  }
-
-  delete(endpoint) {
-
-  }
+    request(url: string|Request, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    get(url: string, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    post(url: string, body: any, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    put(url: string, body: any, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    delete(url: string, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    patch(url: string, body: any, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    head(url: string, options?: RequestOptionsArgs) : Observable<Response>{
+    }
+    options(url: string, options?: RequestOptionsArgs) : Observable<Response>{
+    }
 
 }
